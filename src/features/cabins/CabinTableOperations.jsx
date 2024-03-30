8ef24a490c8b95d43c15187c281abcce386546c1
Ziad Ahmed <ziadahmedcf@gmail.com>
2024-03-30 22:38:36 +0200

    feat: added sorting by discount functionality

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -20,6 +20,8 @@ function CabinTableOperations() {
           { value: "name-desc", label: "Sort by name (Z-A)" },
           { value: "regularPrice-asc", label: "Sort by price (low to high)" },
           { value: "regularPrice-desc", label: "Sort by price (high to low)" },
+          { value: "discount-asc", label: "Sort by discount (low to high)" },
+          { value: "discount-desc", label: "Sort by discount (high to low)" },
           { value: "maxCapacity-asc", label: "Sort by capacity (low to hight)" },
           { value: "maxCapacity-desc", label: "Sort by capacity (high to low)" },
         ]}
