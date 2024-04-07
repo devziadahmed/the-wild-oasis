fa0da02a45339c28f21ecc16c27ecd11d18639e0
Ziad Ahmed <ziadahmedcf@gmail.com>
2024-04-07 03:45:52 +0200

    feat: Added Server side filtering / modified the api to be more dynamic

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 function useBookings() {
+  const [searchParam] = useSearchParams();
+
+  const filterValue = searchParam.get("status");
+
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue, filterMethod: "eq" };
+
   const {
     data: bookings,
     error,
     isLoading,
   } = useQuery({
-    queryFn: getBookings,
-    queryKey: ["bookings"],
+    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter],
   });
 
   return { bookings, isLoading, error };
