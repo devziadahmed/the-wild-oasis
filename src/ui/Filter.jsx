78463a228b85e240d7c8eebdbfcd6631c959b7ce
Ziad Ahmed <ziadahmedcf@gmail.com>
2024-04-15 03:06:40 +0200

    fix: Reseted the page to 1 on filtering to avoid requesting undefined data

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -41,6 +41,7 @@ function Filter({ filterField, options }) {
 
   function handleClick(value) {
     searchParam.set(filterField, value);
+    if (searchParam.get("page")) searchParam.set("page", 1);
     setSearchParam(searchParam);
   }
 
