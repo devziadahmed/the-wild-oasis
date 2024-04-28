94845c424cb750dad443f36ea171e0e5b436c6cf
Ziad Ahmed <ziadahmedcf@gmail.com>
2024-04-29 00:30:16 +0300

    feat: Added Error boundaries for development

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { StyleSheetManager } from "styled-components";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ui/ErrorFallback.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StyleSheetManager shouldForwardProp={() => true}>
     <React.StrictMode>
-      <App />
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => window.location.replace("/")}
+      >
+        <App />
+      </ErrorBoundary>
     </React.StrictMode>
   </StyleSheetManager>
 );
