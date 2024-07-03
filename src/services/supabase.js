3a7f8350666d7a9ea6c49d1377b2d6e061d51336
Ziad Ahmed <ziadahmedcf@gmail.com>
2024-07-04 01:06:22 +0300

    Added environment variables

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://zvnpluafwmsxgqimngmk.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2bnBsdWFmd21zeGdxaW1uZ21rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk4MzcxMzcsImV4cCI6MjAyNTQxMzEzN30.IvL2RtoQKKp4GUbnzHj8WhnIck_JnowJRqAdmQ3QtPw";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export { supabaseUrl };
